f0d35fe5f7b2f3c2e2d64848bf5389e66960f824
Colin787 <colinpark4@gmail.com>
2017-06-21 03:34:02 +0000

    finished mygrations

---
+++ migrations/20170621024349_add_famous_person_id_to_milestones.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('milestones', (table) => {
+    table.integer("famous_person_id").references('famous_people.id');
+  });
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('milestones', (table) => {
+    table.dropColums("famous_person_id");
+  });
+};
